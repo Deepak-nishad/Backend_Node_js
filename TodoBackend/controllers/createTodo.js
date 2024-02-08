833162e1694cc3e0570739a06f384d2ad79f8e5e
Deepak-nishad <deepakkumar62559@gmail.com>
2024-02-08 12:03:35 +0530

    Todo App backend

---
+++ TodoBackend/controllers/createTodo.js
@@ -0,0 +1,31 @@
+// CommonJS syntax
+const Todo = require("../models/Todo");
+// route handler
+exports.createTodo = async (req, res) => {
+    try {
+        //extract title and desxcription from reauest body
+        const { title, description } = req.body;
+        console.log(title);
+        console.log(description);
+        //create a new Todo Obj and insert in DB
+        const response = await Todo.create({ title, description });
+        //send a json response with a success flag
+        res.status(200).json(
+            {
+                success: true,
+                data: response,
+                message: 'Entry Created Successfully'
+            }
+        );
+    }
+    catch (err) {
+        res.status(500)
+            .json({
+                success: false,
+                data: "internal server error",
+                message: err.message,
+            })
+    }
+}
+
+
